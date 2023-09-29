eae4a4c368f4a617827d632e4f3cc267cea89f6d
Nitu Balwani <nitu.balwani@avanade.com>
2023-09-29 15:09:03 +0800

    updated

--- AzureFunctionCiCd/Function1.cs
+++ AzureFunctionCiCd/Function1.cs
@@ -7,29 +7,64 @@ using Microsoft.Azure.WebJobs.Extensions.Http;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.Logging;
 using Newtonsoft.Json;
+using SixLabors.ImageSharp.Formats;
+using SixLabors.ImageSharp.PixelFormats;
+using SixLabors.ImageSharp;
+using System.Collections.Generic;
+using SixLabors.ImageSharp;
+using SixLabors.ImageSharp.PixelFormats;
+using SixLabors.ImageSharp.Processing;
+using SixLabors.ImageSharp.Formats;
+
 
 namespace AzureFunctionCiCd
 {
     public static class Function1
     {
-        [FunctionName("Function1")]
-        public static async Task<IActionResult> Run(
-            [HttpTrigger(AuthorizationLevel.Function, "get", "post", Route = null)] HttpRequest req,
-            ILogger log)
+        [FunctionName("ResizeImage")]
+        [StorageAccount("StorageConnString")]
+        public static void Run([BlobTrigger("images/{name}")] Stream image,
+        [Blob("thumbnails/{name}", FileAccess.Write)] Stream imageSmall)
         {
-            log.LogInformation("C# HTTP trigger function processed a request.");
+            IImageFormat format = Image.DetectFormat(image);
 
-            string name = req.Query["name"];
 
-            string requestBody = await new StreamReader(req.Body).ReadToEndAsync();
-            dynamic data = JsonConvert.DeserializeObject(requestBody);
-            name = name ?? data?.name;
 
-            string responseMessage = string.IsNullOrEmpty(name)
-                ? "v1 : This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response."
-                : $"v1 : Hello, {name}. This HTTP triggered function executed successfully.";
-
-            return new OkObjectResult(responseMessage);
+            using (Image<Rgba32> input = Image.Load<Rgba32>(image))
+            {
+                ResizeImage(input, imageSmall, ImageSize.Small, format);
+            }
         }
+
+
+
+        public static void ResizeImage(Image<Rgba32> input, Stream output, ImageSize size,
+            IImageFormat format)
+        {
+            var dimensions = imageDimensionsTable[size];
+
+
+
+            input.Mutate(x => x.Resize(dimensions.Item1, dimensions.Item2));
+            input.Save(output, format);
+        }
+
+
+
+        public enum ImageSize { ExtraSmall, Small, Medium }
+
+
+
+        private static Dictionary<ImageSize, (int, int)> imageDimensionsTable =
+            new Dictionary<ImageSize, (int, int)>() {
+                { ImageSize.ExtraSmall, (320, 200) },
+                { ImageSize.Small,      (640, 400) },
+                { ImageSize.Medium,     (800, 600) }
+        };
+
+
+
     }
 }
+    
+
